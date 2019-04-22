299ca3c10edbadb2f695d035279158f644a8ed11
506683059@qq.com <506683059@qq.com>
2019-04-22 17:26:01 +0800

    使用FastJson代替Jackson解析返回数据

---
+++ src/main/java/com/github/zk/springbootrediscache/configuration/HttpConverterConfig.java
@@ -0,0 +1,43 @@
+package com.github.zk.springbootrediscache.configuration;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zk
+ * @date 2018/8/16 14:09
+ * 使用fastjson代替jackson
+ */
+@Configuration
+public class HttpConverterConfig {
+
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        // 1.定义一个converters转换消息的对象
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        // 2.添加fastjson的配置信息，比如: 是否需要格式化返回的json数据
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        //null值返回
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.WriteMapNullValue);
+        //处理中文乱码
+        List<MediaType> fastMediaTypes = new ArrayList<>();
+        fastMediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        fastConverter.setSupportedMediaTypes(fastMediaTypes);
+
+        // 3.在converter中添加配置信息
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        // 4.将converter赋值给HttpMessageConverter
+        HttpMessageConverter<?> converter = fastConverter;
+        // 5.返回HttpMessageConverters对象
+        return new HttpMessageConverters(converter);
+    }
+}
